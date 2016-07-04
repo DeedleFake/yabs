22f517998af26cd36b58cd0d6e23fa2fd017359d
DeedleFake <deedlefake@users.noreply.github.com>
2016-07-04 10:58:35 -0400

    Initial Commit.

---
+++ README.md
@@ -0,0 +1,6 @@
+yabs
+====
+
+Yet Another Btrfs Snapshotter.
+
+### Warning: yabs is in early development and is *not functional*. Don't bother using it yet.
---
+++ yabs.go
@@ -0,0 +1,44 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"os"
+)
+
+func globalUsage(flagSet *flag.FlagSet) {
+	fmt.Fprintf(os.Stderr, "Usage: %v [global options] <command> [command options]\n", os.Args[0])
+	fmt.Fprintln(os.Stderr)
+
+	fmt.Fprintln(os.Stderr, "Global options:")
+	flagSet.PrintDefaults()
+	fmt.Fprintln(os.Stderr)
+
+	fmt.Fprintln(os.Stderr, `Commands:
+  update
+        Updates a snapshot timeline.
+
+Follow a command with --help for more detailed information.`)
+}
+
+func main() {
+	var flags struct {
+		cRoot string
+	}
+
+	flagSet := flag.NewFlagSet(os.Args[0], flag.ExitOnError)
+	flagSet.Usage = func() { globalUsage(flagSet) }
+	flagSet.StringVar(&flags.cRoot, "c", "/etc/yabs/snaps.d/", "The dir to look for configs in.")
+	flagSet.Parse(os.Args[1:])
+
+	switch cmd := flagSet.Arg(0); cmd {
+	case "help", "":
+		flagSet.Usage()
+		os.Exit(2)
+
+	default:
+		fmt.Fprintf(os.Stderr, "Unknown command: %q\n", cmd)
+		flagSet.Usage()
+		os.Exit(2)
+	}
+}

8bf0cb99cb56fa9d59f13fa48875d579215c6591
DeedleFake <deedlefake@users.noreply.github.com>
2016-07-06 11:43:12 -0400

    Make Snapshot() actually check for an existing destination as documented.

--- btrfs.go
+++ btrfs.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"os"
 	"os/exec"
 )
@@ -14,6 +15,16 @@ const (
 // it writable. If dst already exists, the snapshot is not created and
 // an error is returned.
 func Snapshot(src, dst string, rw bool) error {
+	_, err := os.Stat(dst)
+	switch {
+	case os.IsNotExist(err):
+
+	case err == nil:
+		return &SnapshotDestExistsError{dst}
+	default:
+		return err
+	}
+
 	args := make([]string, 0, 6)
 	args = append(args, "btrfs", "subvolume", "snapshot")
 	if !rw {
@@ -27,4 +38,12 @@ func Snapshot(src, dst string, rw bool) error {
 	btrfs.Stderr = os.Stderr
 
 	return btrfs.Run()
+}
+
+type SnapshotDestExistsError struct {
+	Dest string
+}
+
+func (err SnapshotDestExistsError) Error() string {
+	return fmt.Sprintf("Snapshot destination %q already exists", err.Dest)
 }
